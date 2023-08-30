84a81b24aee8c5a04215af3e7f3c808c9843cd7a
eleanor-woodhouse <eleanor.woodhouse@gmail.com>
2023-08-30 15:29:39 +1200

    add vercel analytics

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,5 @@
 import React, { MouseEvent, useEffect, useRef } from "react"
+import { Analytics } from "@vercel/analytics/react"
 import { useState } from "react"
 import "../styles/App.scss"
 import { getRandomImageWithShowDetails } from "../runway"
@@ -223,100 +224,104 @@ function App() {
   }
 
   return (
-    <div className="main-body">
-      <div
-        className={`transition-grid ${transitionGridIsVisible ? "visible" : "invisible"} ${
-          animationIsPaused ? "no-transition" : ""
-        }`}
-        style={transitionGridSizes}
-        onTransitionEnd={handleTransitionEnd}
-      >
-        <div className="cell-one"></div>
-        <div className="cell-two"></div>
-        <div className="cell-three"></div>
-        <div className="cell-four"></div>
-        <div className="cell-five"></div>
-        <div className="cell-six"></div>
-        <div className="cell-seven"></div>
-        <div className="cell-eight"></div>
-        <div className="left-gutter"></div>
-        <div className="top-gutter"></div>
-        <div className="middle-cell"></div>
-        <div className={`right-gutter ${gridIsClosed ? "closed" : ""}`}></div>
-        <div className={`bottom-gutter ${gridIsClosed ? "closed" : ""}`}></div>
-      </div>
-      <div className={`loading ${loadingCardIsVisible ? "visible-loading" : "invisible-loading"}`}>
-        <div className={`loading-card ${loadingCardIsVisible ? "visible-loading-card" : "invisible-loading-card"}`}>
-          <div className="loading-text">Loading</div>
+    <>
+      <div className="main-body">
+        <div
+          className={`transition-grid ${transitionGridIsVisible ? "visible" : "invisible"} ${
+            animationIsPaused ? "no-transition" : ""
+          }`}
+          style={transitionGridSizes}
+          onTransitionEnd={handleTransitionEnd}
+        >
+          <div className="cell-one"></div>
+          <div className="cell-two"></div>
+          <div className="cell-three"></div>
+          <div className="cell-four"></div>
+          <div className="cell-five"></div>
+          <div className="cell-six"></div>
+          <div className="cell-seven"></div>
+          <div className="cell-eight"></div>
+          <div className="left-gutter"></div>
+          <div className="top-gutter"></div>
+          <div className="middle-cell"></div>
+          <div className={`right-gutter ${gridIsClosed ? "closed" : ""}`}></div>
+          <div className={`bottom-gutter ${gridIsClosed ? "closed" : ""}`}></div>
         </div>
-      </div>
-      <div className="container" onClick={handleClick}>
-        <div className="left-gutter" ref={leftGutterRef}></div>
-        <div className="top-gutter" ref={topGutterRef}></div>
-        <div className="middle-cell" ref={middleCellRef}>
-          {imageAndDetails.image ? (
-            <>
-              <div className={`info-pane ${vogueLinkIsVisible ? "mobile-click" : ""}`}>
-                <div className="card-container">
-                  <button className="info-card" onClick={handleInfoClick}>
-                    <a
-                      className="info-card-link"
-                      href={imageAndDetails.externalLink}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <ul className="info">
-                        <li>Designer: {imageAndDetails.designer}</li>
-                        <li>Season: {imageAndDetails.season}</li>
-                        <li>City: {imageAndDetails.city}</li>
-                        <li className="smaller">Photo: {imageAndDetails.image.credit}</li>
-                      </ul>
-                      <p className="vogue-link">view the show on vogue.com &gt;</p>
-                    </a>
-                  </button>
-                  <a
-                    href={imageAndDetails.externalLink}
-                    className={`vogue-link-mobile-container ${vogueLinkIsVisible ? "is-visible" : ""}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={handleVogueLinkClick}
-                  >
-                    <button className="vogue-link-mobile">view the show on vogue.com &gt;</button>
-                  </a>
-                </div>
-              </div>
-              <img src={imageAndDetails.image.url} alt={imageAndDetails?.name} onLoad={imageLoaded} />
-            </>
-          ) : (
-            <div className="error-message">something went wrong, please try again</div>
-          )}
-          <div className={`about-text ${aboutIsVisible ? "visible-about-text" : ""}`} onClick={handleAboutClick}>
-            <p>Welcome</p>
-            <p> I am a runway-look random image generator</p>
-            <p>
-              Click anywhere on the screen to retrieve a completely new look from vogue.com
-              <br></br>
-              The archive is vast, so it may take a moment or two for a new image to be decided upon during each journey
-            </p>
-            <p>I am not sure how long it will take Vogue to notice I exist - please enjoy me while you can</p>
+        <div className={`loading ${loadingCardIsVisible ? "visible-loading" : "invisible-loading"}`}>
+          <div className={`loading-card ${loadingCardIsVisible ? "visible-loading-card" : "invisible-loading-card"}`}>
+            <div className="loading-text">Loading</div>
           </div>
         </div>
-        <div
-          className="about-cell"
-          onClick={handleAboutClick}
-          onMouseEnter={() => {
-            setAboutIsVisible(true)
-          }}
-          onMouseLeave={() => {
-            setAboutIsVisible(false)
-          }}
-        >
-          ?
+        <div className="container" onClick={handleClick}>
+          <div className="left-gutter" ref={leftGutterRef}></div>
+          <div className="top-gutter" ref={topGutterRef}></div>
+          <div className="middle-cell" ref={middleCellRef}>
+            {imageAndDetails.image ? (
+              <>
+                <div className={`info-pane ${vogueLinkIsVisible ? "mobile-click" : ""}`}>
+                  <div className="card-container">
+                    <button className="info-card" onClick={handleInfoClick}>
+                      <a
+                        className="info-card-link"
+                        href={imageAndDetails.externalLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <ul className="info">
+                          <li>Designer: {imageAndDetails.designer}</li>
+                          <li>Season: {imageAndDetails.season}</li>
+                          <li>City: {imageAndDetails.city}</li>
+                          <li className="smaller">Photo: {imageAndDetails.image.credit}</li>
+                        </ul>
+                        <p className="vogue-link">view the show on vogue.com &gt;</p>
+                      </a>
+                    </button>
+                    <a
+                      href={imageAndDetails.externalLink}
+                      className={`vogue-link-mobile-container ${vogueLinkIsVisible ? "is-visible" : ""}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      onClick={handleVogueLinkClick}
+                    >
+                      <button className="vogue-link-mobile">view the show on vogue.com &gt;</button>
+                    </a>
+                  </div>
+                </div>
+                <img src={imageAndDetails.image.url} alt={imageAndDetails?.name} onLoad={imageLoaded} />
+              </>
+            ) : (
+              <div className="error-message">something went wrong, please try again</div>
+            )}
+            <div className={`about-text ${aboutIsVisible ? "visible-about-text" : ""}`} onClick={handleAboutClick}>
+              <p>Welcome</p>
+              <p> I am a runway-look random image generator</p>
+              <p>
+                Click anywhere on the screen to retrieve a completely new look from vogue.com
+                <br></br>
+                The archive is vast, so it may take a moment or two for a new image to be decided upon during each
+                journey
+              </p>
+              <p>I am not sure how long it will take Vogue to notice I exist - please enjoy me while you can</p>
+            </div>
+          </div>
+          <div
+            className="about-cell"
+            onClick={handleAboutClick}
+            onMouseEnter={() => {
+              setAboutIsVisible(true)
+            }}
+            onMouseLeave={() => {
+              setAboutIsVisible(false)
+            }}
+          >
+            ?
+          </div>
+          <div className="right-gutter" ref={rightGutterRef}></div>
+          <div className="bottom-gutter" ref={bottomGutterRef}></div>
         </div>
-        <div className="right-gutter" ref={rightGutterRef}></div>
-        <div className="bottom-gutter" ref={bottomGutterRef}></div>
       </div>
-    </div>
+      <Analytics />
+    </>
   )
 }
 
